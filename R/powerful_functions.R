120704ca87c3f5ad9fc2c60af8b13210e699829e
unknown <uajey@wiwi-pool44.wiwi.kit.edu>
2015-07-27 21:49:35 +0200

    Value functions to value_functions, powerful_function ready.

--- R/powerful_functions.R
+++ R/powerful_functions.R
@@ -4,6 +4,7 @@
 #' one specific user only at a time. The powerful_functionallows you to run almost all other functions in this package with the same parameters you are used to
 #' use, but for more users at once. The difference being that for \code{userid} you can now enter just one ID or a vector of IDs contained in your data.
 #'
+#' If you enter more parameters that a given function needs, the function will just take the parameters it needs and ignore the extra ones.
 #' Important is that you understand, the parameters of the function are passed on to other functions of the package, so the following description of
 #' parameters applies to the functions you are going to input in the \code{FUN} parameter, rather than to the powerful_function itself. You can imagine
 #' this function as being a loop-function that takes any other function and runs it for more \code{userid}s than just one.
@@ -13,7 +14,7 @@
 #'
 #' @param userid an integer that gives the information of which user you want the data from: User ID.
 #'
-#' @param FUN character. Name of the function within the package you want to use.
+#' @param FUN Name of the function within the package you want to use. Input without "".
 #'
 #' @param attr attributes IDs, vector of integer numbers corresponding to the attributes you desire to use; attr are assumed to be 1-indexed.
 #'
@@ -43,15 +44,25 @@
 #' (0-index works best for 'round'), atid = integer column for referring the attribute ID (1 indexed), selected = numeric value of the attribute for a specific, given round,
 #' selectable = amount of options the user can chose at a given round, with the current configuration. This is a necessary parameter.
 #'
-#' \code{userid}
+#' \code{userid} is a necessary parameter, without it you'll get a warning. Default is NULL.
 #'
-#' \code{FUN} Default value is "decision_matrix".
+#' \code{FUN} Default value is "decision_matrix". Some functions you can try \code{overall_pv gain_loss_matrices gain_matrix loss_matrix norm_g_l_matrices
+#' decision_matrix get_attrs_ID get_table_by_ID get_rounds_by_ID ref_points}
 #'
 #' \code{rounds} Default calculates first round(initia product config) and last round of the session.
 #' Default calculates with first and last attributes (initial and final product configuration). To choose all give "all" as argument
 #' for rounds, see example. "first" and "last" are also possible argument values. You can give a vector of arbitrarily chosen rounds as well.
 #'
-#' \code{weight} dfedf
+#' \code{refps} If you only want to see the results for one attribute you may enter only a couple of reference points
+#' but you have to tell the function which attributes you want to use those referene points for. So the amount of attr and of refps should be the same.
+#' Moreover the functions always orders de attr, so be sure to input the reference point also in an ascending order corresponding to their attributes. (refps
+#' will not be ordered)
+#'
+#' \code{cost_ids} Default assumes all your attributes are of benefit type, that is a higher value in the attribute means the user
+#' is better of than with a lower value. If one or more of the attributes in your data is of cost type, e.g. price, so that lower is better then you should identify
+#' this attributes as such, providing their id, they'll be converted to benefit type (higher amount is better).
+#'
+#' \code{weight}
 #'
 #' \code{alpha} Default value as given by Reference [1] is 0.88
 #'
@@ -61,7 +72,13 @@
 #'
 #' @return The same value that returns the function you entered in \code{FUN} for the users you entered in the \code{userid} parameter.
 #' @examples
-#' powerful_function(data_camera, 2)
+#' powerful_function(data_camera, 2)  # FUN=decision_matrix
+#' powerful_function(data_pc, userid = c(1,2,4,8,9,100))
+#' powerful_function(my_table, get_all_userids(my_table))
+#'
+#' powerful_function(data_pc, userid = c(1,2,3), FUN = gain_matrix)
+#' powerful_function(data_pc, userid = c(1,2,3), FUN = overall_pv, alpha = 0.9, weight=c(0.1,0.2,0.5,0.2))
+#' powerful_function(datadata, c(6,9), FUN = ref_points) # Returns the default values for both users (user6, user9) assumed as reference points.
 #'
 #' @export
 
@@ -77,7 +94,7 @@ powerful_function <- function(dataset, userid = NULL, FUN = decision_matrix, att
       ##Create conditions
       con1 <- (identical(FUN, get_attrs_ID) | identical(FUN, get_all_userids))
       con2 <- (identical(FUN, get_table_by_ID) | identical(FUN, get_rounds_by_ID) | identical(FUN, get_all_default_rps)
-               | identical(FUN, read_attr_levels))
+               | identical(FUN, get_attr_values))
       con3 <- identical(FUN, decision_matrix)
       con4 <- identical(FUN, ref_points)
       con5 <- (identical(FUN, gain_matrix) | identical(FUN, loss_matrix) | identical(FUN, norm_g_l_matrices))
@@ -123,71 +140,6 @@ powerful_function <- function(dataset, userid = NULL, FUN = decision_matrix, att
     stop("You need to provide at least 1 userid.")
   }
 
-}
-##ready
-pvalue_matrix <- function(dataset, userid = NULL, attr = NULL, rounds = NULL, refps = NULL, cost_ids = NULL,
-                          alpha = 0.88, beta = 0.88, lambda = 2.25) {
-  ngain <- norm_g_l_matrices(dataset, userid, attr, rounds, refps, cost_ids)$ngain
-  nloss <- norm_g_l_matrices(dataset, userid, attr, rounds, refps, cost_ids)$nloss
-
-  v_matrix <- prospect_value_matrix_extend(ngain, nloss, alpha, beta, lambda)
-  v_matrix
-
-}
-
-
-
-##TODO y pv_matrix by original data and then usable on powerful_function
-prospect_value_matrix_extend <- function(ngain = NULL, nloss = NULL,alpha = 0.88, beta = 0.88, lambda = 2.25)  {
-  if((is.null(ngain) || is.null(nloss)) ) {
-    stop("You need to provide both normalized gain and loss matrices. Helpful functions: norm_g_l_matrices, gain_matrix,
-        loss_matrix, gain_loss_matrices")
-  }
-  if(!identical(dim(ngain), dim(nloss))) {
-    stop("The input matrices do not have equal dimensions.")
-  }
-  else {
-    value_matrix <- matrix(NA, nrow(ngain), ncol(ngain))
-
-    for(n in 1:nrow(ngain)) {
-      value_matrix[n, ] <- mapply(pvalue_fun, ngain[n, ], nloss[n, ], alpha, beta, lambda)
-    }
-  }
-  value_matrix
-}
-
-
-##explain alpha, beta, lambda and digits = 2
-pvalue_fun <- function(ngain_ij, nloss_ij, alpha = 0.88, beta = 0.88, lambda = 2.25) {
-  result <- ((ngain_ij)^alpha) + (-lambda*((-nloss_ij)^beta))
-  result
-
-}
-
-overall_pv <- function (dataset, userid = NULL, attr = NULL, rounds = NULL, refps = NULL, cost_ids = NULL,  weight = NULL,
-                        alpha = 0.88, beta = 0.88, lambda = 2.25) {
-
-  v_matrix <- pvalue_matrix(dataset, userid, attr, rounds, refps, cost_ids, alpha, beta, lambda)
-
-  overall_pv <- overall_pv_extend(v_matrix, weight, dataset)
-
-  overall_pv
-
-}
-
-
-##TO THINK  value_matrix and/or dataset? If you want to weight with rel_frequency you need to provide the dataset
-overall_pv_extend <- function(value_matrix, weight = NULL, wfrom_dataset) {
-  if(is.null(weight)) {
-    result <-get_attr_weight(wfrom_dataset)
-  }
-  else {
-    if(length(weight) != ncol(value_matrix)) {
-      warning("Length of weight does not equal amount of attributes, some recycling may have been done here.")
-    }
-    result <- apply(value_matrix, 1, function(my_vector) { sum(my_vector*weight)})
-  }
-  result
 }
 
 
