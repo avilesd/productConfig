d10542152ae2eed27b86ee9f4dbae99907e85417
avilesdiego <diego8fcb@hotmail.com>
2016-01-11 18:51:49 +0100

    Testing git cache

---
+++ R/BA_TODO.R
@@ -0,0 +1,7 @@
+##' NEWTODO - testing cache git
+##'
+##' BA-Liste
+##'
+##' MostImportant--Vectorize functions, meaning that when given two or more IDs,
+##'       then able to calculate it, without powerful_function.
+##'
