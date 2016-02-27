53d9b19a9204d1201078674a751a8029db2e0d01
Avilés Escobar <uajey@student.kit.edu>
2016-02-27 18:35:08 +0100

    Add new test for to-be deprecated function, but necessary to test newer's results

--- tests/testthat/test-equal-weights.R
+++ tests/testthat/test-equal-weights.R
@@ -19,7 +19,7 @@ test_that("results are numerically the same", {
   amount.eq.WeightAndAttr <- sample(1:length(all.attr), 1)
   randomAttr <- sample(all.attr, amount.eq.WeightAndAttr) # sort? possible error?
   randomWeightVector <- runif(amount.eq.WeightAndAttr, 0.0 , 500.0)
-  print(amount.eq.WeightAndAttr)
+  differFromAttr <- runif(abs(amount.eq.WeightAndAttr-1), 0.0 , 500.0)
 
   legacy.oPV <- unname(powerful_function(camera2_config, randomUsers, rounds=randomRounds, FUN=overall_pv))
   renewed.oPV <- unname(overallPV(camera2_config, randomUsers, rounds=randomRounds))
@@ -28,23 +28,16 @@ test_that("results are numerically the same", {
   expect_identical(legacy.oPV, renewed.oPV)
 
   # deprecated_FUN : Expect same results with random weight parameters, with length(weightVector) == length(attr)
-  legacy.sameLength <- unname(powerful_function(camera2_config, userid = randomUsers, rounds=randomRounds, FUN=overall_pv, attr = randomAttr, weight = randomWeightVector))
-  new.sameLength <- unname(overallPV(camera2_config, userid = randomUsers, attr = randomAttr, rounds=randomRounds,  weight = randomWeightVector))
+  legacy.sameLength <- unname(powerful_function(camera2_config, userid = randomUsers, FUN=overall_pv, rounds=randomRounds, attr = randomAttr, weight = randomWeightVector))
+  new.sameLength <- unname(overallPV(camera2_config, userid = randomUsers, attr = sort(randomAttr), rounds=randomRounds,  weight = randomWeightVector))
 
   expect_identical(legacy.sameLength, new.sameLength)
-  print(randomUsers)
-  print(randomAttr)
-  print(randomWeightVector)
-  print(head(legacy.sameLength))
-  print(head(new.sameLength))
-
 
   # deprecated_FUN: Expect some errors, first controlled then random
     # Controlled
   expect_error(powerful_function(camera2_config, 9:11, FUN = overall_pv, weight=1:3))
   expect_error(overallPV(camera2_config, 9:11, weight=1:3))
     # Random with different length
-  #expect_error(overall_pv(camera2_config, userid = randomUsers, attr = randomAttr,  weight = randomWeightVector))
-  #expect_error(overallPV(camera2_config, userid = randomUsers, attr = randomAttr,  weight = randomWeightVector))
-
+  expect_error(overall_pv(camera2_config, userid = randomUsers, attr = randomAttr,  weight = differFromAttr))
+  expect_error(overallPV(camera2_config, userid = randomUsers, attr = randomAttr,  weight = differFromAttr))
 })
