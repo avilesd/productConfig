8b3495e7d1ca845ab2d420604cf5e96196212536
avilesdiego <diego8fcb@hotmail.com>
2016-03-02 12:02:11 +0100

    1. Added ability to correctly handle cost-type attribute, including correctly transforming the tri refps, also when entering more than one attr. 2. Small fix for avoiding lapply error when hmaxVector empty

--- R/TRPFunctions.R
+++ R/TRPFunctions.R
@@ -8,6 +8,9 @@
 
 ## DOCU cost_ids still works pretty well, but the tri.refps also have to be changed, explain it on BA
 ## with a nice diagram, mr and g exchange values and change from positive/negative sign.
+# cost_ids, enter normal reference points, we will convert them. cost_ids has to equal the attribute you are inputting
+
+#' DOCU: you can enter cost_ids normally, program will recognize for which attr it should use the cost_ids
 trpValueMatrix <- function(dataset, userid = NULL, attr = NULL, rounds = NULL, refps = NULL, cost_ids = NULL,
                             mr = 0.5, sq = 1.5, g = 2.5, beta_f = 5, beta_l = 1, beta_g = 1, beta_s = 3) {
   counter <- 0
@@ -18,12 +21,14 @@ trpValueMatrix <- function(dataset, userid = NULL, attr = NULL, rounds = NULL, r
   else {
     for(i in attr) {
       if (counter == 0) {
-        trp.list <- trpValueMatrix.oneAttr(dataset, userid, attr=i, rounds, refps, cost_ids,
+        if(i %in% cost_ids) cost_ids_help <- i else cost_ids_help <- NULL
+        trp.list <- trpValueMatrix.oneAttr(dataset, userid, attr=i, rounds, refps, cost_ids_help,
                                            mr, sq, g, beta_f, beta_l, beta_g, beta_s)
         counter <- 1
       }
       else {
-        tempVariable <- trpValueMatrix.oneAttr(dataset, userid, attr=i, rounds, refps, cost_ids,
+        if(i %in% cost_ids) cost_ids_help <- i else cost_ids_help <- NULL
+        tempVariable <- trpValueMatrix.oneAttr(dataset, userid, attr=i, rounds, refps, cost_ids_help,
                                                mr, sq, g, beta_f, beta_l, beta_g, beta_s)
         trp.list <- mapply(cbind, trp.list, tempVariable, SIMPLIFY = F)
       }
@@ -60,11 +65,25 @@ substract_sq <- function(x, status_quo) {
   res
 }
 # AUxiliaary function only.
+#' cost_ids, enter normal reference points, we will convert them. cost_ids has to equal the attribute you are inputting
+#' DOCU: Converting tri.refps, no need to convert if attribute is of type cost.
 trpValueMatrix.oneAttr <- function(dataset, userid = NULL, attr = NULL, rounds = NULL, refps = NULL, cost_ids = NULL,
                            mr = 0.5, sq = 1.5, g = 2.5, beta_f = 5, beta_l = 1, beta_g = 1, beta_s = 3) {
-  if(length(attr)!= 1) {
-    stop("Please insert (only) one attribute ID.")
+
+  if(length(attr)!= 1) stop("Please insert (only) one attribute ID.")
+
+  if(!is.null(cost_ids)) {
+    old.mr <- (-1)*mr
+    mr <- (-1)*g
+    g <- old.mr
+    sq <- (-1)*sq
+    print(mr)
+    print(sq)
+    print(g)
   }
+
+  #if(!is.null(cost_ids) & cost_ids != attr) warning("attribute and cost_ids should be for the same attribute...")
+
   # First Transformation, monotonic transformation such that SQ = 0
   # Transform decision Matrix
   list.decMatrices <- decisionMatrix(dataset, userid, attr, rounds, cost_ids)
@@ -81,7 +100,7 @@ trpValueMatrix.oneAttr <- function(dataset, userid = NULL, attr = NULL, rounds =
 
   # Second Transformation, normalize, first normalize matrices
   hmaxVector <- lapply(list.decMatrices, function(temp) apply(temp, 2, abs))
-  hmaxVector <- lapply(hmaxVector, function(temp1) apply(temp1, 2, max))
+  hmaxVector <- lapply(hmaxVector, function(temp1) if(is.null(ncol(temp1))) {temp1} else {apply(temp1, 2, max)})
   hmaxVector <- lapply(hmaxVector, function(temp2) replace(temp2, temp2==0.0, 1.00)) #remove 0 to avoid NA when dividing
 
   list.decMatrices <- mapply(function(aMatrix, aVector) aMatrix / aVector[col(aMatrix)], list.decMatrices, hmaxVector, SIMPLIFY = F)
@@ -91,6 +110,7 @@ trpValueMatrix.oneAttr <- function(dataset, userid = NULL, attr = NULL, rounds =
   tri.refps <- lapply(hmaxVector, function(temp, temp2) temp2/temp, tri.refps)
   valueMatrix <- mapply(trpValueFunction, list.decMatrices, tri.refps, SIMPLIFY = F)
   valueMatrix
+
   #print(hmaxVector)
   #print(tri.refps)
   #print(list.decMatrices)
@@ -102,6 +122,31 @@ trpValueMatrix.oneAttr <- function(dataset, userid = NULL, attr = NULL, rounds =
   #list.valueMatrices
 }
 
+#' New function as interface with weights and your trp.valueMatrix
+#' Docu: For the way the trp function works it is a little more complicated than for overallPV for the pt
+#' here we have to manually calculate the AttributeWeights whit your desired function, e.g ww <- getAttrWeights(...)
+#' and the trp.ValueMatrix separately as well, trp.VM <- mapply() OR trpValueMatrix(...)
+#' then giving to this function as input and getting the desired result
+
+trp.overallPV_extend <- function (trp.ValueMatrix, weight = NULL) {
+
+  if(is.null(weight) | is.null(trp.ValueMatrix)) {
+    stop("You need to provide both arguments: trp.ValueMatrix and their weights")
+  }
+
+  tryCatchResult = tryCatch({
+    trp.overallPV <- mapply(overall_pv_extend, trp.ValueMatrix, weight, SIMPLIFY = F) ##Perhaps mapply when data.frame, make weights as list?!
+
+  }, warning = function(condition) {
+    message("Probably amount of users differs from amount of weightVectors and they cannot be recycled.")
+    message("Result most likely not accurrate, check your arguments.")
+  }, error = function(condition) {
+    stop("The input in the weight parameters can be flexible but check if you are entering the right amount of weightVectors, users and attributes")
+  }, finally={
+  })
+
+  trp.overallPV
+}
 
 #Delete:
 trpValueMatrix22 <- function(dataset, userid = NULL, attr = NULL, rounds = NULL, refps = NULL, cost_ids = NULL,
