eff9130e6f7e13a7241dbbc15edea0897ee89349
avilesdiego <diego8fcb@hotmail.com>
2016-03-04 19:08:52 +0100

    Change mr, sq , g in a test with a cost attribute so that mr > sq > g

--- tests/testthat/test-equal-trp-vMatrices.R
+++ tests/testthat/test-equal-trp-vMatrices.R
@@ -10,7 +10,7 @@ test_that("results are identical", {
   trp10.4 <- readRDS("C:/Users/diego/Google Drive/KIT/Seminar_eC_HCI/R/productConfig/data/trp10-4.rds")
 
   result13 <- trpValueMatrix(camera2_config, 10, attr=1:3, rounds="all")
-  result4 <- trpValueMatrix(camera2_config, 10, attr=4, rounds="all", mr=-0.10, sq=0.17, g= 0.40, cost_ids=4) #Input as with actual tri.refps
+  result4 <- trpValueMatrix(camera2_config, 10, attr=4, rounds="all", mr=0.40, sq=0.17, g=-0.10, cost_ids=4) #Input as with actual tri.refps
 
   expect_identical(trp10.13, result13)
   expect_identical(trp10.4, result4)
