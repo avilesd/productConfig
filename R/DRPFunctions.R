47835fcecbe8dec084c4918e2a342ba6ed864e25
avilesdiego <diego8fcb@hotmail.com>
2016-03-04 19:02:21 +0100

    Add new R script 'DRPFUnctions.R' to develop dual reference model value function(s)

---
+++ R/DRPFunctions.R
@@ -0,0 +1,6 @@
+# Dual reference point:
+# New set of functions for implementing the Dual Reference Model (SQ, Aspirations) and the overall prospect values
+# x + ln(x) + ((10/(1+(1/e^(0.5*(x+ln(x)-5+ln(5))))))-(10/(1+(1/e^(0.5*(3+ln(3)-5+ln(5)))))))
+# ((10/(1+(1/e^(0.5*(4+ln(4)-15+ln(15))))))-(10/(1+(1/e^(0.5*(3+ln(3)-15+ln(15))))))) x = 4, sq= 3 result = 0.143286
+# ((10/(1+(1/e^(0.5*(2+ln(2)-15+ln(15))))))-(10/(1+(1/e^(0.5*(3+ln(3)-15+ln(15))))))) x = 2, sq= 3 result = -0.08188
+
