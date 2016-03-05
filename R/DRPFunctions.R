c57e880ea56a5d7b18fa6e6ec8d262b8d8edce21
avilesdiego <diego8fcb@hotmail.com>
2016-03-05 19:12:12 +0100

    Implement fully: dual reference model value function, pending cons_fun

--- R/DRPFunctions.R
+++ R/DRPFunctions.R
@@ -4,6 +4,33 @@
 # ((10/(1+(1/e^(0.5*(4+ln(4)-15+ln(15))))))-(10/(1+(1/e^(0.5*(3+ln(3)-15+ln(15))))))) x = 4, sq= 3 result = 0.143286
 # ((10/(1+(1/e^(0.5*(2+ln(2)-15+ln(15))))))-(10/(1+(1/e^(0.5*(3+ln(3)-15+ln(15))))))) x = 2, sq= 3 result = -0.08188
 
+dualValueMatrix <- function(dataset, userid = NULL, attr = NULL, rounds = NULL, cost_ids = NULL,
+                            dual.refps = c(sq=NA, g=NA), consumption_fun = NULL, lambda = 2.25, delta = 0.8) {
+  counter <- 0
+  if (length(attr) == 1) {
+    dual.list <- dualValueMatrix.oneAttr(dataset, userid, attr, rounds, cost_ids,
+                                       dual.refps, consumption_fun, lambda, delta)
+  }
+  else {
+    if (is.null(attr)) attr <- get_attrs_ID(dataset)
+    for(i in attr) {
+      if (counter == 0) {
+        if(i %in% cost_ids) cost_ids_help <- i else cost_ids_help <- NULL
+        dual.list <- dualValueMatrix.oneAttr(dataset, userid, attr=i, rounds, cost_ids_help,
+                                             dual.refps, consumption_fun, lambda, delta)
+        counter <- 1
+      }
+      else {
+        if(i %in% cost_ids) cost_ids_help <- i else cost_ids_help <- NULL
+        tempVariable <- dualValueMatrix.oneAttr(dataset, userid, attr=i, rounds, cost_ids_help,
+                                                 dual.refps, consumption_fun, lambda, delta)
+        dual.list <- mapply(cbind, dual.list, tempVariable, SIMPLIFY = F)
+      }
+    }
+  }
+  dual.list
+}
+
 dualValueMatrix.oneAttr <- function(dataset, userid = NULL, attr = NULL, rounds = NULL, cost_ids = NULL,
                                     dual.refps = c(sq=NA, g=NA), consumption_fun = NULL, lambda = 2.25, delta = 0.8) {
   sq <- dual.refps[1]
@@ -16,21 +43,30 @@ dualValueMatrix.oneAttr <- function(dataset, userid = NULL, attr = NULL, rounds 
     # This function does not checks if sq < g, since not required in the model
     g <- (-1)*g
     sq <- (-1)*sq
+    dual.refps <- c(sq, g)
   }
+
+  list.decMatrices <- decisionMatrix(dataset, userid, attr, rounds, cost_ids)
 
   if (is.null(consumption_fun)) {
     # Check if negative, through peeking data and cost_ids (already applied in dM), cost_ids not need explicit.
     # When all positive procede with f1 and then f2
-    list.decMatrices <- decisionMatrix(dataset, userid, attr, rounds, cost_ids)
     valueMatrices <- lapply(list.decMatrices, smallerThanZero, dual.refps, lambda, delta)
-    valueMatrices
+  }
+  else { # Write in comments, no transformation will be carried on, negative values accepted
+         # user will have to write its own function as an argument (function(t) ...)
+    #gainLossMatrices <- lapply(list.decMatrices, gainLoss.consumption, dual.refps, consumption_fun)
+    #valueMatrices <- lapply(gainLossMatrices, lossAversion.consumption, list.decMatrices, dual.refps, lambda, delta, consumption_fun)
 
   }
-  else {
-    # Do not transform anything, cost_ids act as usual since negative values are accepted
-  }
+  valueMatrices
 }
-#### Delete later: THis comment marks the state before uniting dual function with smallterThanZero
+#### Delete later: THis comment marks the state before uniting dual function
+#with smallterThanZero
+#@smallerThanZero, we decided to normalize just after calculating gains and
+#losses, it is not 100% reliable, but much better than normalizing before (raw
+#data) or after(value matrix) because you loose the information about 1.
+#distance to the sq and 2. loss aversion effect, respectivly.
 smallerThanZero <- function(aMatrix, dual.refps, lambda = 2.25, delta = 0.8) {
   if(all(aMatrix > 0) & all(dual.refps > 0)) {}
     # aMatrix and dual.refps stay the same
@@ -48,13 +84,17 @@ smallerThanZero <- function(aMatrix, dual.refps, lambda = 2.25, delta = 0.8) {
      dual.refps <- dual.refps + absOfSmallestPlus1
   }
   gainLossMatrix <- dualGainLossFunction(aMatrix, dual.refps)
-  dual.vMatrix <- dualLossAversionFun(gainLossMatrix, aMatrix, lambda, delta)
+  hmaxVector <-  max(abs(gainLossMatrix)) #since no 0 possible, replacing 0 with 1 is not necessary
+  norm.gainLossMatrix <- gainLossMatrix/hmaxVector
+  norm.gainLossMatrix
+
+  dual.vMatrix <- dualLossAversionFun(norm.gainLossMatrix, aMatrix, lambda, delta)
   dual.vMatrix
 
   #Call from here and return valueMatrix, give argument to return gain? or another equal function?
 }
 
-dualGainLossFunction <- function(aMatrix, dual.refps, lambda = 2.25, delta = 0.8) {
+dualGainLossFunction <- function(aMatrix, dual.refps) {
   x <- aMatrix
   if(any(x < 0)) warning("x should not be smaller than 0.")
   sq <- dual.refps[1]
@@ -69,10 +109,25 @@ dualLossAversionFun <- function(gainLossMatrix, aMatrix, lambda = 2.25, delta = 
   valueM <- yMatrix + xMatrix
   valueM
 }
+####################
+####################
+#gainLoss.consumption <- function(aMatrix, dual.refps, cons_fun) {
+#  x <- aMatrix
+#  sq <- dual.refps[1]
+#  g <- dual.refps[2]
+#  result <- (10/(1+(1/exp(0.5*(cons_fun(x)-cons_fun(g))))))-(10/(1+(1/exp(0.5*(cons_fun(sq)-cons_fun(g))))))
+#  result
+#}
+#lossAversion.consumption <- function(gainLossMatrix, aMatrix, lambda = 2.25, delta = 0.8, cons_fun) {
+#  yMatrix <- apply(gainLossMatrix, 1:2, function(y) if(y < 0) {delta*lambda*y} else {delta*y})
+#  xMatrix <- apply(aMatrix, 1:2, function(x) (1-delta)*(cons_fun(x)))
+#  valueM <- yMatrix + xMatrix
+#  valueM
+#}
 
 #Vectorialised if empty, returns NA
-greaterThanZero <- function(x) {
-  result <- x[x <= 0]
-  if (length(result) == 0) result <- NA
-  result
-}
+#greaterThanZero <- function(x) {
+#  result <- x[x <= 0]
+#  if (length(result) == 0) result <- NA
+#  result
+#}
--- tests/testthat/test-dual-gainLossFunction_basic.R
+++ tests/testthat/test-dual-gainLossFunction_basic.R
@@ -17,12 +17,35 @@ test_that("results are identical", {
   #$`9`
   #[1] 5 1
 
+  #gainLoss should - dualValueMatrix(myData, 10, attr=1:3, rounds="all",  dual.refps = c(1.5,2.5))
+  #       attr1      attr2      attr3
+  #0round 2.3845413 -2.9314944 -2.9314944
+  #1round 0.9509677 -2.9314944 -2.9314944
+  #2round 0.9509677  0.7409121 -2.9314944
+  #3round 0.9509677 -0.8659935 -2.9314944
+  #4round 0.9509677 -0.8659935 -0.8659935
+
+  #normalized
+  #$`10`
+  #attr1      attr2      attr3
+  #0round 1.0000000 -1.0000000 -1.0000000
+  #1round 0.3988053 -1.0000000 -1.0000000
+  #2round 0.3988053  0.2527421 -1.0000000
+  #3round 0.3988053 -0.2954103 -1.0000000
+  #4round 0.3988053 -0.2954103 -0.2954103
+
   #Test value matrix dM(myData, 11, attr=3, rounds="all", dual = c(1.5, 2.5))
   vm11_3 <- readRDS("C:/Users/diego/Google Drive/KIT/Seminar_eC_HCI/R/productConfig/data/vm11_3.rds")
   dm11_3 <- readRDS("C:/Users/diego/Google Drive/KIT/Seminar_eC_HCI/R/productConfig/data/dm11_3.rds")
   expectedVM <- dualValueMatrix.oneAttr(camera2_config, 11, attr=3, rounds="all", dual = c(1.5, 2.5))
 
-  expect_identical(vm11_3, smallerThanZero(dm11_3[[1]], c(1.5,2.5), lambda=2.25, delta=0.8))
-  expect_identical(vm11_3, expectedVM[[1]])
+  # No longer expecting identical, since this was made before normalizing
+  #expect_identical(vm11_3, smallerThanZero(dm11_3[[1]], c(1.5,2.5), lambda=2.25, delta=0.8))
+  #expect_identical(vm11_3, expectedVM[[1]])
+
+  # Before normalizing dual : overallPV_interface(dual10.full, weight = c(0.25,.25,.25,.25))
+  #$`10`
+  #0round      1round      2round      3round      4round
+  #-1.79124087 -2.32285703 -0.44540967 -1.13409607 -0.03942965
 
 })
