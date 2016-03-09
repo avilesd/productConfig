df2ef146f22cfd52b8e6e1a04246c31d80e72943
avilesdiego <diego8fcb@hotmail.com>
2016-03-09 19:47:18 +0100

    Complete @differenceToIdeal and implement on @getAttrWeights, partially tested and docu started

--- R/WeightFunctions.R
+++ R/WeightFunctions.R
@@ -70,11 +70,14 @@ getAttrWeights <- function(dataset = NULL, userid = NULL, weight = NULL,  attr =
     }
     ## DOCU: New functions must take into account attributes and calculate accordingly, perhaps it doesn't make sense with our data,
     ## but we have to give the choice
-    if (weightFUN == "Test1") {
-      result <- weight_higher_sum_value(dataset, userid, rounds, cost_ids)
+    if (weightFUN == "differenceToIdeal") {
+      result <- weight.differenceToIdeal(dataset, userid, attr, rounds, cost_ids)
     }
     if (weightFUN == "Test2") {
-      result <- weight_higher_sum_value(dataset, userid, rounds, cost_ids)
+      result <- weight_higher_sum_value(dataset, userid, attr, rounds, cost_ids)
+    }
+    if (weightFUN == "Test3") {
+      result <- weight_higher_sum_value(dataset, userid, attr, rounds, cost_ids)
     }
   }
   else { result <- weight}
@@ -92,28 +95,92 @@ getAttrWeights <- function(dataset = NULL, userid = NULL, weight = NULL,  attr =
 #nsumdiffMatrix11 <- apply(ndiffMatrix11, 2, sum)
 #nsumdiffMatrix11 <- apply(nabsdiffMatrix11, 2, sum)
 
-weight.differenceToIdeal <- function(dataset, userid = NULL , attr = NULL, rounds = NULL, cost_ids = NULL) {
+#' Title
+#'
+#' @param dataset
+#' @param userid
+#' @param attr
+#' @param rounds Default is rounds to have more data to calculate with. If you
+#'   enter 'first' or' last' there is only one rounds to gather data from so
+#'   consequently all attribtues will have the same weight.
+#' @param cost_ids
+#'
+#' @details This function rewards attributes which values do not change much
+#'   throughtout the decision matrix, even if the value is the lowest value.For
+#'   an opposite implicit effect see \code{\link{}}
+#'
+#'   \code{cost_ids} As in the other functions, if you enter a cost_ids that is
+#'   not in your entered attributes, the functions will calculate the output
+#'   with all attributes in your data, including the cost(s) attributes and only
+#'   after the calculations does the function subset the result according to the
+#'   \code{attr} input. When the attributes and cost_ids differ, the function
+#'   allows the calculation but it will throw a warning.
+#'
+#' @return
+#'
+#' @examples
+#'
+#' @references [1]Ma, J., Fan, Z. P., & Huang, L. H. (1999). A subjective and
+#'   objective integrated approach to determine attribute weights. European
+#'   journal of operational research, 112(2), 397-404.
+#'
+#'   [2] Fan, Z. P. (1996). Complicated multiple attribute decision making:
+#'   theory and applications (Doctoral dissertation, Ph. D. Dissertation,
+#'   North-eastern University, Shenyang, PRC).
+#' @export
 
-  ##Calculate with always 4 attribute so that function works properly, take attr into account at result level.
+weight.differenceToIdeal <- function(dataset, userid = NULL , attr = NULL, rounds = "all", cost_ids = NULL) {
 
+  ##Calculate with always 4 attribute so that function works properly, take attr into account at result level. Good idea, since we
+  ## cut with [, x] in normalize.altMethod
+  # Common Errors catched in dM (Tested: attr, rounds, all.users, cost_ids)
+  if (!all(cost_ids %in% attr)) warning("One of your cost_ids is not in your attributes. Is this still your intended result?")
+  if (is.null(attr)) attr <- get_attrs_ID(dataset)
 
+  decisionList <- decisionMatrix(dataset, userid, attr = NULL, rounds)
+  normList <- lapply(decisionList, normalize.altMethod, attr, cost_ids)
+  weightList <- lapply(normList, differenceToIdeal, attr)
+  weightList <- lapply(weightList, function(t) t[attr])
+  weightList
 }
 
+#' Title
+#'
+#' @param aMatrix if matrix is only one row, then according to method in [1], no
+#'   change to ideal, so all weights will be given the same weight, that is, 1/length(attr)
+#'
+#' @param attr
+#' @param cost_ids
+#'
+#' @return
+#' @export
+#'
+#' @examples
 normalize.altMethod <- function(aMatrix, attr, cost_ids) {
-
-    aMatrix[,cost_ids] <- apply(aMatrix[,cost_ids], 2, function(t) { a_max <- max(t); a_min <- min(t); res <- (a_max-t)/(a_max-a_min); res})
+  if (nrow(aMatrix) == 1) {
+    aMatrix <- apply(aMatrix, 1:2, function(t) 1 )
+  }
+  else{
+    aMatrix[,cost_ids] <- apply(aMatrix[,cost_ids, drop = F], 2, function(t) { a_max <- max(t); a_min <- min(t);
+    if (a_max == 0 & a_min == 0) {a_max <- 1; a_min <- 1}
+    if (a_max ==  a_min) {res <- t/a_max}
+    else {res <- (a_max-t)/(a_max-a_min)}; res})
 
     benefitAttr <- attr[!attr %in% cost_ids]
-    print(benefitAttr)
-    aMatrix[ ,benefitAttr] <- apply(aMatrix[ ,benefitAttr], 2, function(t) { a_max <- max(t); a_min <- min(t); res <- (t-a_min)/(a_max-a_min); res})
-
-    aMatrix
+    aMatrix[,benefitAttr] <- apply(aMatrix[,benefitAttr, drop = F], 2, function(t) { a_max <- max(t); a_min <- min(t);
+    if (a_max == 0 & a_min == 0) {a_max <- 1; a_min <- 1}
+    if (a_max ==  a_min) {res <- t/a_max}
+    else {res <- (t-a_min)/(a_max-a_min)}; res})
+  }
+  aMatrix
 }
 
-differenceToIdeal <- function(normalizedMatrix) {
+differenceToIdeal <- function(normalizedMatrix, attr) {
   vector2_0 <- apply(normalizedMatrix, 2, function(t) { b_max <- max(t); sumOfDiff <- (sum((b_max - t)^2))  })
+  vector2_0 <- replace(vector2_0, vector2_0==0.0, 1/(length(attr)/2)) # Avoid divide by zero, the 2 regulates the huge impact a none-changing value has on the weights
   vector2_1 <- 1/vector2_0
   vector3 <- sum(vector2_1)
+  vector3 <- replace(vector3, vector3==0.0, 1)
   weightVector <- vector2_1/vector3
   weightVector
 }
