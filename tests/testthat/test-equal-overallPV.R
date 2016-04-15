76402a2dfaf9076aca73c958f8b60a42ba4e529f
Diego Aviles <diego8fcb@hotmail.com>
2016-04-15 22:04:47 +0200

    75% finish 4.1 Evaluating pt, drp, trp. Save 15.04.2016-1

--- tests/testthat/test-equal-overallPV.R
+++ tests/testthat/test-equal-overallPV.R
@@ -14,6 +14,12 @@ test_that("results are identical, with static weight", {
   overall_new <- unname(overallPV(camera2_config, randomUsers, rounds=randomRounds, weight = c(0.25,0.25,0.25,0.25)))
   overall_legacy <- unname(powerful_function(camera2_config, randomUsers, FUN=overall_pv, rounds=randomRounds ,weight=c(0.25,0.25,0.25,0.25)))
 
+  #fulle.dual.refps <- c(1.50,  2.5 ,1.50,  2.5 ,1.50,  2.5 ,0.17, -0.1)
+  #highestDRP <- getHighestRound(overallDRP(myData, 11, attr=1:4, rounds="all", cost_ids = 4, dual.refps=full.dual.refps))
+  #highestPV <- getHighestRound(overallPV(myData, all.users, attr=1:4, rounds="all", cost_ids = 4, refps = c(1.5,1.5,1.5,0.17)))
+
   expect_identical(overall_new, overall_legacy)
+
+  #compare(highestDRP, highestPV)
 
 })
